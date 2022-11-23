b64ffe0eece0b4c9d896d39f1194a29f0a21f220
jhonatta costa <jhonattan.ramon.17@gmail.com>
2022-11-23 14:14:56 -0300

    Update JHON.js
    commit 2

--- JHON.js
+++ JHON.js
@@ -4,3 +4,11 @@ let jhonPASSOUemAPS = true
 if (jhonPASSOUemAPS) {
     console.log('PARABENS NÂO FEZ MAIS QUE OBRIGAÇÂO');
 }
+
+let minhaNota = 8
+let mediaParaPassar = 6
+
+while(minhaNota > mediaParaPassar){
+    console.log('PASSOU, falta pouco para terminar');
+    
+}
