2dfc407bc94c528307a66907183fd8c97acc519b
jhonatta costa <jhonattan.ramon.17@gmail.com>
2022-11-23 14:46:22 -0300

    jhon
    v3

--- JHON.js
+++ JHON.js
@@ -8,3 +8,10 @@ if (jhonPASSOUemAPS) {
     console.log('PARABENS NÂO FEZ MAIS QUE OBRIGAÇÂO');
 }
 
+let minhaNota = 8
+let mediaParaPassar = 6
+
+while(minhaNota > mediaParaPassar){
+    console.log('PASSOU, falta pouco para terminar');
+    minhaNota--
+}
